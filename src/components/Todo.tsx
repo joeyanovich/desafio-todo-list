035acc250fe586fda45e251d5153d652ac4b0e32
joeyanovich <joedison.dias@gmail.com>
2023-09-25 20:16:03 -0300

    feat 🧑‍💻: created input field

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,12 @@
-// import styles from './Todo.module.css'
+import styles from './Todo.module.css'
 
 import { PlusCircle } from "@phosphor-icons/react";
 
 export function Todo() {
   return (
     <div>
-      <form action="">
-        <input type="text" />
+      <form  className={styles.form} action="">
+        <input type="text" placeholder="Adicione uma nova tarefa"/>
         <button type="submit">
           Criar
           <PlusCircle size={20} />
